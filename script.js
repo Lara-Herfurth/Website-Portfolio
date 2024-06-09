ab1305902c612a845c47cd1c3b64b0851022063d
tabuenam <juan.jose.mejia@hs-furtwangen.de>
2024-06-09 22:24:01 +0200

    https://github.com/Lara-Herfurth/portfolio.git

---
+++ script.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const projectItems = document.querySelectorAll('.project-item');
+
+    const appearOptions = {
+        threshold: 0.1,
+        rootMargin: "0px 0px -100px 0px"
+    };
+
+    const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll) {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("show");
+            } else {
+                entry.target.classList.remove("show");
+            }
+        });
+    }, appearOptions);
+
+    projectItems.forEach(project => {
+        appearOnScroll.observe(project);
+    });
+});
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const links = document.querySelectorAll('a');
+    const transitionElement = document.querySelector('.page-transition');
+
+    links.forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            let target = this.getAttribute('href');
+
+            transitionElement.classList.add('active');
+
+            setTimeout(() => {
+                window.location.href = target;
+            }, 1000); // Duration should match the CSS transition duration
+        });
+    });
+});
+
